b98d80901c3b2fb9ac055a354c01bb9676a2f679
alex daniel baena yepez <alex.baena@est.iudigital.edu.co>
2024-09-02 10:16:09 -0500

    primer commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+Título del proyecto.
+Descripción: Cuenta un poco de qué trata tu proyecto,su función, concepto y/o objetivos. En esta parte puedes utilizar capturas de pantalla para ser más específico.
+Retos y dificultades: Puedes comentar cuáles fueron los desafíos que tuviste que enfrentar a la hora de realizar este proyecto.
+
+Cómo fueron resueltos estos retos.
+Tus logros: Partes del proyecto que sientas orgullo de haber logrado, incluso puedes adjuntar bloques del código.
+Estado del proyecto: Especifica si seguirás trabajando o ya está finalizado.
+
+
+echo "# Empleado_Aris" >> README.md 
+git init 
+git add README.md 
+git commit -m "primer commit" 
+git branch -M main 
+git remote add origin https://github.com/alexdanielbaenayepez/Empleado_Aris.git
+ git push -u origin main
---
+++ version_02.py
@@ -0,0 +1,33 @@
+# Import libraries
+import streamlit as st
+import pandas as pd
+
+
+st.set_page_config(page_title="Python Talks Search Engine", page_icon="🐍", layout="wide")
+st.title("Datos empleados Aris") #css color #192547
+
+df = pd.read_excel('Titanic.xlsx').fillna("")
+
+text_search = st.text_input("Ingrese el Nombre o la Cedula ", value="")
+   # Test: Moran, Mr. James
+
+#    st.write(df)  --> imprime todo el dataframe
+
+
+
+to_numeric = 0
+if text_search.isnumeric():
+   to_numeric =  pd.to_numeric(text_search, downcast='integer')
+   st.write(df.loc[df.PassengerId == to_numeric])
+   # Test: 543
+ #KeyError: "None of [Index([7], dtype='object')] are in the [columns]"
+ # este es el error actual mas cercano que tengo a un buen resultado
+
+
+
+df_search = df[df["Name"].str.contains(text_search)]
+if text_search.__contains__(" "):# Crear una expresion regular que acepte mayusculas, minusculas y espacios
+   st.write(df_search)
+
+
+
